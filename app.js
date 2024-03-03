c621a016677411e18a7ba525c417a99d40fe0e7f
jessie758 <jessie272758@gmail.com>
2024-03-03 21:23:57 +0800

    feat: 編輯餐廳

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000;
 const db = require('./models');
 const Restaurant = db.Restaurant;
 
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 const { engine } = require('express-handlebars');
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
@@ -78,11 +81,34 @@ app.get('/restaurants/:id', (req, res) => {
     .catch((err) => console.log(err));
 });
 
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id;
+  const form = {
+    action: `/restaurants/${id}?_method=PUT`,
+    method: 'POST',
+    title: '編輯餐廳',
+    type: 'edit',
+  };
+
+  return Restaurant.findByPk(id, { raw: true })
+    .then((restaurant) => res.render('form', { form, ...restaurant }))
+    .catch((err) => console.log(err));
+});
+
 app.post('/restaurants', (req, res) => {
   const data = req.body;
 
   return Restaurant.create({ ...data })
     .then(() => res.redirect('/restaurants'))
+    .catch((err) => console.log(err));
+});
+
+app.put('/restaurants/:id', (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+
+  return Restaurant.update({ ...data }, { where: { id } })
+    .then(() => res.redirect(`/restaurants/${id}`))
     .catch((err) => console.log(err));
 });
 
