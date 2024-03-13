35f9afae40b8e1865797e920e2774c5eade765bd
jessie758 <jessie272758@gmail.com>
2024-03-13 17:15:10 +0800

    feat: 透過 dotenv 套件加載環境變數

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+console.log('NODE_ENV:', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -24,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(
   session({
-    secret: 'ThisIsSecret',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
